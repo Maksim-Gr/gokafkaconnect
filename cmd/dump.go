ded7b8c6a7d892205d0cc98d5a70090bd93ccab1
Maksim Grishchenko <grischenko1988@gmail.com>
2025-07-19 20:13:28 +0300

    chore: add dump logic for retrieving config

--- cmd/dump.go
+++ cmd/dump.go
@@ -4,10 +4,12 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"gokafkaconnect/connector"
 )
+
+var outputFile string
 
 // dumpCmd represents the dump command
 var dumpCmd = &cobra.Command{
@@ -20,7 +22,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dump called")
+		cfg, err := LoadConfig()
+		if err != nil {
+			color.Red("Failed to load config: %v\n", err)
+			return
+		}
+		connectors, err := connector.ListConnectors(cfg.KafkaConnectURL)
+		if err != nil {
+			color.Red("Failed to dump  connector config: %v\n", err)
+			return
+		}
+		err = connector.DumpConnectorConfig(cfg.KafkaConnectURL, connectors, outputFile)
+		if err != nil {
+			color.Red("Failed to dump connector config: %v\n", err)
+			return
+		}
+		color.Green("Successfully dumped connector config to config.json")
 	},
 }
 
@@ -36,4 +53,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// dumpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	dumpCmd.Flags().StringVarP(&outputFile, "output", "o", "current_config.json", "Output file for dumped configurations")
 }
