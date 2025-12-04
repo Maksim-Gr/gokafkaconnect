ee257a13f345da1e1835b6b516661f84e3a4163b
Maksim Grishchenko <grischenko1988@gmail.com>
2025-12-04 20:26:31 +0300

    chore: move structs to the different file

--- internal/connector/api.go
+++ internal/connector/api.go
@@ -9,22 +9,6 @@ import (
 	"os"
 )
 
-type Status struct {
-	Name      string `json:"name"`
-	Connector struct {
-		State    string `json:"state"`
-		WorkerID string `json:"worker_id"`
-	} `json:"connector"`
-	Tasks []struct {
-		ID       int    `json:"id"`
-		State    string `json:"state"`
-		WorkerID string `json:"worker_id"`
-	} `json:"tasks"`
-	Type string `json:"type"`
-}
-
-type ConnectorsStatusResponse map[string]Status
-
 func ListConnectors(kafkaConnectURL string) ([]string, error) {
 	url := fmt.Sprintf("%s/connectors", kafkaConnectURL)
 	req, err := http.NewRequest("GET", url, nil)
